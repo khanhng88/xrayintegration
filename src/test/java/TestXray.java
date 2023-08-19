28c81e5932e850f4260882b352bf265a55c350b1
khanh <emma.devn@gmail.com>
2023-08-19 15:39:30 +0700

    commit AP id

--- src/test/java/TestXray.java
+++ src/test/java/TestXray.java
@@ -4,6 +4,7 @@ Created by emman at 8/18/2023 7:10 PM
 
 import app.getxray.xray.testng.annotations.Requirement;
 import app.getxray.xray.testng.annotations.XrayTest;
+import io.qameta.allure.TmsLink;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -13,12 +14,14 @@ public class TestXray {
 
     @Test
     @XrayTest(key = "AP-48")
+    @TmsLink("AP-48")
     public void test() {
         System.out.println("Khanh test Xray");
     }
 
     @Test
     @XrayTest(key = "AP-49")
+    @TmsLink("AP-49")
     public void test2() {
         System.out.println("Khanh test Xray");
         Assert.assertTrue(false);
