b89f282571d93489cbc14e4d088e679566086fa7
khanh <emma.devn@gmail.com>
2023-08-24 21:03:23 +0700

    removed Tms link

--- src/test/java/TestXray.java
+++ src/test/java/TestXray.java
@@ -21,14 +21,12 @@ public class TestXray {
 
     @Test
     @XrayTest(key = "AP-48")
-    @TmsLinks({@TmsLink("AP-48"), @TmsLink("AP-49")})
     public void test() {
         System.out.println("Khanh test Xray");
     }
 
     @Test
-    @XrayTest(key = "AP-49")
-    @TmsLink("AP-49")
+    @XrayTest(key = "AP-49, AP-53, AP-59")
     public void test2() {
         System.out.println("Khanh test Xray");
         Assert.assertTrue(false);
