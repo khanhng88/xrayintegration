187e8e723dc46b20d74360cd4a5fcdc1e5340603
khanh <emma.devn@gmail.com>
2023-08-24 21:00:10 +0700

    add before suit and After test

--- src/test/java/TestXray.java
+++ src/test/java/TestXray.java
@@ -6,11 +6,18 @@ import app.getxray.xray.testng.annotations.XrayTest;
 import io.qameta.allure.TmsLink;
 import io.qameta.allure.TmsLinks;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 @Listeners({app.getxray.xray.testng.listeners.XrayListener.class})
 public class TestXray {
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("before suite");
+    }
 
     @Test
     @XrayTest(key = "AP-48")
@@ -25,5 +32,10 @@ public class TestXray {
     public void test2() {
         System.out.println("Khanh test Xray");
         Assert.assertTrue(false);
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        System.out.println("After test");
     }
 }
