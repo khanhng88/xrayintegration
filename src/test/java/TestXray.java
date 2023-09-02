eb9ab75f262093d7f01a64667b12b38397567f5f
khanh <emma.devn@gmail.com>
2023-09-02 20:07:29 +0700

    commit new test steps

--- src/test/java/TestXray.java
+++ src/test/java/TestXray.java
@@ -3,6 +3,7 @@ Created by emman at 8/21/2023 4:14 PM
 */
 
 import app.getxray.xray.testng.annotations.XrayTest;
+import io.qameta.allure.Step;
 import io.qameta.allure.TmsLink;
 import io.qameta.allure.TmsLinks;
 import org.testng.Assert;
@@ -23,17 +24,35 @@ public class TestXray {
     @XrayTest(key = "AP-48,AP-49, AP-53,")
     public void test() {
         System.out.println("Khanh test Xray");
+        step_1();
     }
 
     @Test
     @XrayTest(key = " AP-59")
     public void test2() {
         System.out.println("Khanh test Xray");
-        Assert.assertTrue(false);
+        step_2();
+        step_3();
     }
 
     @AfterMethod
     public void afterTest() {
         System.out.println("After test");
+    }
+
+    @Step("Do step 1")
+    public void step_1() {
+        System.out.println("Do step 1");
+    }
+
+    @Step("Do step 2")
+    public void step_2() {
+        System.out.println("Do step 2");
+    }
+
+    @Step("Do step 3")
+    public void step_3() {
+        System.out.println("Do step 3");
+        Assert.assertTrue(false);
     }
 }
